9a5219e748c094ed08bce93b3ef6d3a416a6b00d
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-02-19 15:01:50 +0100

    Controller modified with  @RestController annotation

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 //1. method
-@Controller
-@RequestMapping("/hello")
-@ResponseBody
-//can be declared public also
+@RestController
+
+        //can be declared public also
 class SampleController {
-@GetMapping
-  public String sayHello(){
+  @GetMapping("/hello")
+  public String sayHello() {
     return "Hello";
 
   }
