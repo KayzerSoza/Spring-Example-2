dbe5f987480ae62d1aaab30d53542ee4ff40d62e
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-02-20 12:48:59 +0100

    List of customers added to CustomersController. We want all methods to follow api/ . Therefore we added a @RequestMapping ("/api/") at class level.

--- src/main/java/com/example/demo/CustomersController.java
+++ src/main/java/com/example/demo/CustomersController.java
@@ -1,9 +1,20 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
+@RequestMapping("/api/") // we want api/customers url. So we need to add a default mapping for all the methods below
 public class CustomersController {
 
+  @RequestMapping(value = "/customers", method = GET)
+  public List<Customer> allCustomers(){
+
+    return customersList;
+
+  }
 
 }
