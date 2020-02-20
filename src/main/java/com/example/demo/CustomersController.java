7631227ac788f484ca8bebe3c0822b3202a005bf
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-02-20 13:00:57 +0100

    Customer class is created. customersList is added to CustomersController which will be used as temporary database.

--- src/main/java/com/example/demo/CustomersController.java
+++ src/main/java/com/example/demo/CustomersController.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.ArrayList;
+import java.util.List;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
-@RequestMapping("/api/") // we want api/customers url. So we need to add a default mapping for all the methods below
+// we want api/customers url.
+// So we need to add a default mapping for all the methods below
+@RequestMapping("/api/")
 public class CustomersController {
+  //customersList will function as our database as  long as Controller runs
+  private List<Customer> customersList=new ArrayList<>();
+
 
   @RequestMapping(value = "/customers", method = GET)
   public List<Customer> allCustomers(){
