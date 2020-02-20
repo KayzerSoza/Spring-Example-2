684a6b926c0ab0364d669c849759950d0d24ec53
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-02-21 00:18:03 +0100

    createCustomer method is added

--- src/main/java/com/example/demo/CustomersController.java
+++ src/main/java/com/example/demo/CustomersController.java
@@ -1,25 +1,31 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
-@RestController
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 // we want api/customers url.
-// So we need to add a default mapping for all the methods below
+// So we need to add a default mapping that will affect all methods below
+@RestController
 @RequestMapping("/api/")
 public class CustomersController {
   //customersList will function as our database as  long as Controller runs
-  private List<Customer> customersList=new ArrayList<>();
+  private List<Customer> customersList = new ArrayList<>();
 
 
   @RequestMapping(value = "/customers", method = GET)
-  public List<Customer> allCustomers(){
-
+  public List<Customer> allCustomers() {
     return customersList;
 
+  }
+
+  @RequestMapping(value = "/customers", method = POST)
+  public void createCustomer(@RequestBody Customer customer) {
+    // Without @RequestBody GET method returns null values
+    customersList.add(customer);
   }
 
 }
