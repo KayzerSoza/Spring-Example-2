c2695aed21b11b8ac8ec67bad23812ce8869d55f
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-02-21 19:48:51 +0100

    getOneCustomerById method is added to run get request by id. if id doesn't exist 404 response is given.

--- src/main/java/com/example/demo/CustomersController.java
+++ src/main/java/com/example/demo/CustomersController.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -13,20 +15,20 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 // we want api/customers url.
 // So we need to add a default mapping that will affect all methods below
 @RestController
 @RequestMapping("/api/")
 public class CustomersController {
 
-                                         // Collections.synchronizedList provides thread security
+  // Collections.synchronizedList provides thread security
   private List<Customer> customersList = Collections.synchronizedList(new ArrayList<>());
-  AtomicLong counter =new AtomicLong(); // will increment customerId in a thread secure way
+  AtomicLong counter = new AtomicLong(); // will increment customerId in a thread secure way
 
   @RequestMapping(value = "/customers", method = GET)
   public List<Customer> allCustomers() {
     return customersList;
-
   }
 
   @RequestMapping(value = "/customers", method = POST)
@@ -35,11 +37,24 @@ public class CustomersController {
     customer.setId(counter.addAndGet(1)); // increments counter by delta and gets the value to setId
 
     customersList.add(customer);
-    HttpHeaders headers=new HttpHeaders();
-    headers.add("Location", "/api/customers/"+customer.getId());
-    return new ResponseEntity<>(customer, headers, HttpStatus.CREATED );
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Location", "/api/customers/" + customer.getId());
+    return new ResponseEntity<>(customer, headers, HttpStatus.CREATED);
 
   }
+
+  @RequestMapping(value = "/customers/{id}")
+  public ResponseEntity<Customer> getOneCustomerById(@PathVariable long id) {
+    var customerOptional = customersList.stream()
+            .filter(customer -> customer.getId() == id)
+            .findFirst();
+    return customerOptional
+            .map(customer -> new ResponseEntity<>(customer, HttpStatus.OK))
+            .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+  }
+
+
 
 /*
 // void method adds customer to the list. In this case Get Method returns empty list
